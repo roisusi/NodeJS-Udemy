1b4fcd0d4b03c3f3a06e6f9a7e90aa5892ddb182
Roi Susi <roisusi@gmail.com>
2022-04-01 17:29:49 +0300

    Done Section 8

--- section6/models/tourModel.js
+++ section6/models/tourModel.js
@@ -9,21 +9,44 @@ dotenv.config({ path: './config.env' });
 const mongoose = require('mongoose');
 // const slugify = require('slugify');
 
+const validator = require('validator');
+
 //-----------//
 // Database //
 //----------//
 
+//validators are check in create or in runValidators true in others , that is build in and custom
 const tourSchema = new mongoose.Schema(
   {
-    name: { type: String, required: [true, 'A tour must have a name'], trim: true },
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      trim: true,
+      maxlength: [40, 'A Tour name must have less or equal then 40 chars'],
+      minlength: [10, 'A Tour name must have more or equal then 10 chars'],
+      validate: [validator.isAlpha, 'Tour Name must contain only alphanumeric characters'],
+    },
     duration: { type: Number, required: [true, 'A tour must have a duration'] },
     maxGroupSize: { type: Number, required: [true, 'A tour must have a size'] },
-    difficulty: { type: String, required: [true, 'A tour must have a difficulty'] },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: { values: ['easy', 'medium', 'difficult'], message: 'Difficulty is either : easy, medium, difficult' },
+    },
     rating: { type: Number, default: 4.5 },
-    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsAverage: { type: Number, default: 4.5, min: [1, 'Rating must have above 1.0'], max: [5, 'Rating must have below 5.0'] },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
-    priceDiscount: { type: Number },
+    //custom validator callback function only on create !!!
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: `Cant discount price ({VALUE}) more then regular `,
+      },
+    },
     summary: { type: String, trim: true },
     description: { type: String, trim: true },
     imageCover: { type: String, required: [true, 'A tour must have a cover image'] },
